3b25dcf3fd334aace5cc4b6ad7fd234f480ab3c3
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-10-13 21:49:14 +0530

    added a test for 1 and 2 element array

--- test_assignments.js
+++ test_assignments.js
@@ -1,31 +1,33 @@
 const assert = require("assert");
 const lib = require("./source_code_of_assignments.js");
 //test for the function which picks odd numbers from an array
-//check for a odd number that should give me back that number in an array
+// a odd number that should give me back that number in an array
 assert.deepEqual(lib.pickOddNumbers("1"),[1]);
-//check for a 5 numbers that should give me back odd numbers from that
+// a 5 numbers that should give me back odd numbers from that
 assert.deepEqual(lib.pickOddNumbers("12324"),[1,3]);
-//check for many numbers that should give me back the odd numbers
+// many numbers that should give me back the odd numbers
 assert.deepEqual(lib.pickOddNumbers("12323423"),[1,3,3,3]);
 
 //test for the function which picks even numbers from an array
-//check for one odd number that should give me back an empty array
+// one odd number that should give me back an empty array
 assert.deepEqual(lib.pickEvenNumbers("1"),[]);
-// check for 5 numbers that should give me back even numbers only
+//  5 numbers that should give me back even numbers only
 assert.deepEqual(lib.pickEvenNumbers("12324"),[2,2,4]);
-//check for many numbers that should give me back the even numbers
+// many numbers that should give me back the even numbers
 assert.deepEqual(lib.pickEvenNumbers("12323423"),[2,2,4,2]);
 
 //tests for the functiom which adds all the numbers of list
-//check for one empty list should give me back nothing
+// one empty list should give me back nothing
 assert.deepEqual(lib.calculateSum([]),0);
-//check for 1 element array should give me back that element
+// 1 element array should give me back that element
 assert.deepEqual(lib.calculateSum([1]),1);
-//check for 2 element array should give me back the sum of them
+// 2 element array should give me back the sum of them
 assert.deepEqual(lib.calculateSum([1,2]),3);
 //tests for reversing an array
-//check for empty array should give me back an empty array
+// empty array should give me back an empty array
 assert.deepEqual(lib.reverseArray([]),[]);
-
-
+// 1 element array should give back that element
+assert.deepEqual(lib.reverseArray([2]),[2]);
+// 2 element array should give back the reversed array
+assert.deepEqual(lib.reverseArray([2,3]),[3,2]);
 
