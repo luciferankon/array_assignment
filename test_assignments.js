8d55a75f906bf80786c9c1cff5a32ced01bf1297
Ankon <44019225+luciferankon@users.noreply.github.com>
2018-10-20 09:58:30 +0530

    added filter in picking even or odd numbers function

--- test_assignments.js
+++ test_assignments.js
@@ -2,19 +2,19 @@ const assert = require("assert");
 const lib = require("./source_code_of_assignments.js");
 //test for the function which picks odd numbers from an array
 // a odd number that should give me back that number in an array
-assert.deepEqual(lib.pickOddNumbers("1"),[1]);
+assert.deepEqual(lib.pickOddNumbers([1]),[1]);
 // a 5 numbers that should give me back odd numbers from that
-assert.deepEqual(lib.pickOddNumbers("12324"),[1,3]);
+assert.deepEqual(lib.pickOddNumbers([1,2,3,2,4]),[1,3]);
 // many numbers that should give me back the odd numbers
-assert.deepEqual(lib.pickOddNumbers("12323423"),[1,3,3,3]);
+assert.deepEqual(lib.pickOddNumbers([1,2,3,2,3,4,2,3]),[1,3,3,3]);
 
 //test for the function which picks even numbers from an array
 // one odd number that should give me back an empty array
-assert.deepEqual(lib.pickEvenNumbers("1"),[]);
+assert.deepEqual(lib.pickEvenNumbers([1]),[]);
 //  5 numbers that should give me back even numbers only
-assert.deepEqual(lib.pickEvenNumbers("12324"),[2,2,4]);
+assert.deepEqual(lib.pickEvenNumbers([1,2,3,2,4]),[2,2,4]);
 // many numbers that should give me back the even numbers
-assert.deepEqual(lib.pickEvenNumbers("12323423"),[2,2,4,2]);
+assert.deepEqual(lib.pickEvenNumbers([1,2,3,2,3,4,2,3]),[2,2,4,2]);
 
 //tests for the functiom which adds all the numbers of list
 // one empty list should give me back nothing
